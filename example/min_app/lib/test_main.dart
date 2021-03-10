fb1dd3468d68bda48ab2966e45b88e3fa685a5d4
alex <alex@tekartik.com>
2021-03-10 14:19:54 +0100

    fix test

---
+++ example/min_app/lib/test_main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
+import 'package:tekartik_test_menu_flutter/demo/demo.dart';
+import 'package:tekartik_test_menu_flutter/demo/demo_test_menu_flutter.dart'
+    as demo;
+import 'package:tekartik_test_menu_flutter/test.dart';
+
+void main() {
+  initTestMenuFlutter();
+
+  /*
+  test('crash', () {
+    fail('fail');
+  });
+  */
+  group('group', () {
+    test('success', () {
+      expect(true, isTrue);
+      write('success');
+    });
+  });
+
+  menu('group menus', () {
+    group('group1', () {
+      group('sub_group1', () {
+        test('test', () {});
+      });
+      group('sub_group1', () {
+        test('test', () {});
+      });
+    });
+  });
+
+  demo.main();
+
+  menu('custom', () {
+    item('do it', () {
+      write('ok');
+      write('or no');
+    });
+    test('success', () {
+      expect(true, isTrue);
+      write('success');
+    });
+  });
+  menu('group', () {
+    test('failure', () {
+      fail('failure');
+    });
+
+    test('expect_failure', () {
+      write('failure');
+      expect(true, isFalse);
+    });
+    test('success', () {
+      expect(true, isTrue);
+      write('success');
+    });
+
+    test('throw', () {
+      throw 'error thrown';
+    });
+  });
+  item('root_item', () {
+    write('from root item');
+  });
+  item('sleep 1000', () async {
+    write('before sleep');
+    await sleep(2000);
+    write('after sleep 2000');
+  });
+  item('navigate', () {
+    Navigator.push(buildContext!,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(title: const Text('test')),
+          body: demoSimpleList(context));
+    }));
+  });
+}
--- repo_support/test/min_app_test.dart
+++ repo_support/test/min_app_test.dart
@@ -4,15 +4,17 @@ import 'package:process_run/shell_run.dart';
 import 'package:tekartik_build_utils/android/android_import.dart';
 import 'package:tekartik_build_utils/flutter/app/generate.dart';
 import 'package:tekartik_build_utils/flutter/flutter.dart';
+import 'package:path/path.dart';
 
 void main() {
   group(
     'min_app',
     () {
       test('fs_generate', () async {
-        var dirName = '.dart_tool/test_menu_flutter/test/app/gen_min_app';
+        var dirName = join(
+            '.dart_tool', 'test_menu_flutter', 'test', 'app', 'gen_min_app');
 
-        var src = 'example/min_app';
+        var src = join('..', 'example', 'min_app');
         await fsGenerate(dir: dirName, src: src);
         expect(await File(join(dirName, 'pubspec.yaml')).readAsString(),
             await File(join(src, 'pubspec.yaml')).readAsString());
