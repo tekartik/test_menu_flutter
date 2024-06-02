9bfe003c248886823adda48cf564c578aa8741ad
alex <alex@tekartik.com>
2024-06-02 09:27:35 +0200

    fix ci

--- repo_support/tool/run_ci.dart
+++ repo_support/tool/run_ci.dart
@@ -1,15 +1,13 @@
 import 'package:dev_build/package.dart';
 import 'package:path/path.dart';
-import 'package:process_run/shell.dart';
-import 'package:pub_semver/pub_semver.dart';
 
 Future<void> main() async {
-  if (dartVersion >= Version(2, 12, 0, pre: '0')) {
-    for (var dir in [
-      'test_menu_flutter',
-      'test_menu_flutter_example',
-    ]) {
-      await packageRunCi(join('..', dir));
-    }
+  for (var dir in [
+    'test_menu_flutter',
+    'test_menu_flutter_example',
+    'repo_support',
+    join('example', 'min_app'),
+  ]) {
+    await packageRunCi(join('..', dir));
   }
 }
